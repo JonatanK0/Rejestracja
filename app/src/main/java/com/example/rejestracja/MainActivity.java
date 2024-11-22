731773882a0bcc01d32e82a9e6c5446eae69ddc0
JonatanK0 <kozlowskijonatancs@gmail.com>
2024-11-22 19:19:12 +0100

    Done

--- app/src/main/java/com/example/rejestracja/MainActivity.java
+++ app/src/main/java/com/example/rejestracja/MainActivity.java
@@ -1,24 +1,53 @@
 package com.example.rejestracja;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText Email;
+    private EditText Haslo;
+    private EditText PowtorzHaslo;
+    private TextView Komunikat;
+    private Button Zatwierdz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.editTextText), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Email = findViewById(R.id.email);
+        Haslo = findViewById(R.id.haslo);
+        PowtorzHaslo = findViewById(R.id.powtorzHaslo);
+        Komunikat = findViewById(R.id.Komunikat);
+        Zatwierdz = findViewById(R.id.zatwierdz);
+
+        Zatwierdz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = Email.getText().toString().trim();
+                String haslo = Haslo.getText().toString().trim();
+                String powtorzHaslo = PowtorzHaslo.getText().toString().trim();
+
+                if (email.isEmpty()) {
+                    Komunikat.setText("Proszę podać adres E-Mail.");
+                } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    Komunikat.setText("Niepoprawny adres E-Mail.");
+                } else if (haslo.isEmpty()) {
+                    Komunikat.setText("Proszę podać hasło.");
+                } else if (powtorzHaslo.isEmpty()) {
+                    Komunikat.setText("Proszę powtórzyć hasło.");
+                } else if (!(haslo.equals(powtorzHaslo))) {
+                    Komunikat.setText("Hasło nie jest takie same.");
+                } else {
+                    Komunikat.setText("Witaj ," + email);
+                }
+            }
         });
     }
 }
